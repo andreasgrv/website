7c822ca214d2a93f22be21c6211541bf34372104
grv <grv@ulysses.ithaca>
2015-08-05 14:57:19 +0300

    added initial blog

--- mysite.py
+++ mysite.py
@@ -1,39 +1,99 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
+# based on flask examples
+# static blog example below:
+# http://www.jamesharding.ca/posts/simple-static-markdown-blog-in-flask/
 
 import os
-from flask import Flask, request, redirect, render_template, send_from_directory
+import argparse
+from flask import Flask, render_template, send_from_directory
 from flask_bootstrap import Bootstrap
+from flask_flatpages import FlatPages, pygments_style_defs
+from flask_frozen import Freezer
+
+
+# global options
+DEBUG = True
+FLATPAGES_AUTO_RELOAD = DEBUG
+FLATPAGES_EXTENSION = '.md'
+FLATPAGES_ROOT = 'content'
+POST_DIR = 'posts'
 
 # create our application :)
 app = Flask(__name__)
+# use flatpages for blog
+flatpages = FlatPages(app)
+# apply freezer to generate static pages
+freezer = Freezer(app)
+app.config.from_object(__name__)
+# we want bootstrap!
 Bootstrap(app)
 
 music_dir = 'static/music'
 music_files = [f.split('.')[0] for f in os.listdir(music_dir)
                if f.endswith('.mp3')]
 
-@app.route('/grv')
+
+@app.route('/')
 def homepage():
     return render_template('homepage.html')
 
-@app.route('/grv/about')
+
+@app.route('/posts')
+def posts():
+    """Get the posts from folder
+    :returns: The page of the posts
+
+    """
+    posts = [p for p in flatpages if p.path.startswith(POST_DIR)]
+    posts.sort(key=lambda item: item['date'], reverse=False)
+    return render_template('posts.html', posts=posts)
+
+
+@app.route('/posts/<name>/')
+def post(name):
+    """A page for each post
+
+    :name: The name of the post
+    :returns: The rendered page of this post
+
+    """
+    path = '%s/%s' % (POST_DIR, name)
+    post = flatpages.get_or_404(path)
+    return render_template('post.html', post=post)
+
+
+@app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/grv/read')
+
+@app.route('/read')
 def reading():
     return render_template('read.html')
 
-@app.route('/grv/listen')
+
+@app.route('/listen')
 def listen():
     return render_template('playlist.html', music_dir=music_dir,
                            music_files=enumerate(music_files))
 
-@app.route('/grv/<fname>')
+
+@app.route('/<fname>')
 def download_file(fname):
     return send_from_directory(directory='files', filename=fname,
                                as_attachment=True)
 
+
+@app.route('/pygments.css')
+def pygments_css():
+    return pygments_style_defs('tango'), 200, {'Content-Type': 'text/css'}
+
 if __name__ == '__main__':
-    app.run(port=8080, host='inta.io', debug=False)
+    parser = argparse.ArgumentParser(description="Run server or freeze")
+    parser.add_argument('--freeze', type=bool,
+                        dest='freeze', help='whether to create a static copy')
+    args = parser.parse_args()
+    if args.freeze:
+        freezer.freeze()
+    app.run(host='localhost', port=8000, debug=True)
