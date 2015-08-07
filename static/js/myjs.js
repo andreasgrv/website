eb874a4159dc48d35a8623025933668d8b2c8f67
grv <grv@ulysses.ithaca>
2015-08-07 14:31:11 +0300

    corrections + hack js a bit to avoid printing whole text on load and then typing it

--- static/js/myjs.js
+++ static/js/myjs.js
@@ -1,12 +1,18 @@
 // I am so proud i wrote some js!
-$(function(){
-  $(".typed").typed({
-	strings: [$(".typed").html()],
-	typeSpeed: 10,
-	showCursor: false,
-	contentType: 'html',
-  });
+$(function() {
+	$(".typed").hide();
 });
 $( document ).ready(function() {
 	console.log( "document loaded" );
+	setTimeout(function(){
+	  $(".typed").typed({
+		strings: [$(".typed").html()],
+		typeSpeed: 10,
+		showCursor: false,
+		contentType: 'html',
+	  });
+	}, 100);
+	setTimeout(function(){
+		$(".typed").show();
+	}, 300);
 });
