6722df505720dbdbfe1344f9cb79584cd3bbe14b
grv <andreasgrv@gmail.com>
2016-06-29 00:57:16 +0300

    reinclude js scripts - fix type js
    reinclude accidentally removed scripts after jinja base template change.
    fix typed js moving of content by correctly configuring blocks content
    in css. tidy js up a bit

--- static/js/myjs.js
+++ static/js/myjs.js
@@ -1,33 +1,14 @@
-// I am so proud i wrote some js!
-$(function() {
-	$(".typed").hide();
-});
-
 $( document ).ready(function() {
-	setTimeout(function(){
-	  $(".typed").typed({
-		strings: [$(".typed").html()],
-		typeSpeed: 10,
-		showCursor: false,
-		contentType: 'html',
-	  });
-	}, 100);
-	setTimeout(function(){
-		$(".typed").show();
-	}, 300);
-    $( ".nav li" ).mouseenter(function() {
-        console.log("got here!");
-        $( this ).siblings().each(function() {
-            if ($( this ).hasClass("active")) {
-                $( this ).removeClass("active").addClass("inactive");
-            }
-        });
-    });
-    $( ".nav li" ).mouseleave(function() {
-        $( this ).siblings().each(function() {
-            if ($( this ).hasClass("inactive")) {
-                $( this ).removeClass("inactive").addClass("active");
-            }
-        });
-    });
+    $(".typed").css("visibility", "hidden");
+    setTimeout(function(){
+      $(".typed").typed({
+        strings: [$(".typed").html()],
+        typeSpeed: 10,
+        showCursor: false,
+        contentType: 'html'
+      });
+    }, 100);
+    setTimeout(function(){
+        $(".typed").css("visibility", "visible");
+    }, 300);
 });
