4434957e5572d12fe1e43843f650d34953ac04e8
grv <andreasgrv@gmail.com>
2016-06-29 01:22:12 +0300

    correct typed visibility and add second typer
    sometimes text was visible before typing began.
    
    add another type js process for dates in posts because it has no
    centering issues this way.

--- static/js/myjs.js
+++ static/js/myjs.js
@@ -1,14 +1,26 @@
+$(".typed").css("visibility", "hidden");
+$(".typed2").css("visibility", "hidden");
 $( document ).ready(function() {
-    $(".typed").css("visibility", "hidden");
     setTimeout(function(){
       $(".typed").typed({
         strings: [$(".typed").html()],
-        typeSpeed: 10,
+        typeSpeed: 20,
         showCursor: false,
         contentType: 'html'
       });
     }, 100);
     setTimeout(function(){
         $(".typed").css("visibility", "visible");
-    }, 300);
+    }, 250);
+    setTimeout(function(){
+      $(".typed2").typed({
+        strings: [$(".typed2").html()],
+        typeSpeed: 20,
+        showCursor: false,
+        contentType: 'html'
+      });
+    }, 3000);
+    setTimeout(function(){
+        $(".typed2").css("visibility", "visible");
+    }, 3150);
 });
